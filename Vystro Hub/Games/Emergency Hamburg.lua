36cd3be0bbd995752fdc9c080ef0b81256da67f3
smhaa <166930057+Chromeyc@users.noreply.github.com>
2024-09-20 21:30:54 +0200

    Update Emergency Hamburg.lua

--- Vystro Hub/Games/Emergency Hamburg.lua
+++ Vystro Hub/Games/Emergency Hamburg.lua
@@ -1,2 +1,23 @@
--- Nothing Here Yet
-print("Nothing")
+-- Not Added Yet
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+    Title = 'Vystro | Emergency Hamburg 1.0',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['Main'] = Window:AddTab('Main'),
+}
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Note:')
+
+LeftGroupBox:AddLabel('This Script is not added Yet!')
