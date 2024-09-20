14c9dad132775a75e6283656b3ce5f9c4fd86182
smhaa <166930057+Chromeyc@users.noreply.github.com>
2024-09-20 21:24:40 +0200

    Update main.lua

--- Vystro Hub/Games/main.lua
+++ Vystro Hub/Games/main.lua
@@ -2,7 +2,7 @@
 
 -- // Emergency Hamburg \\
 if game.PlaceId == 7711635737 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Chromeyc/roblox/main/Vystro%20Hub/Games/Arsenal.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chromeyc/roblox/main/Vystro%20Hub/Games/Emergency%20Hamburg.lua"))()
 end
 -- // Arsenal \\
 if game.PlaceId == 286090429 then
