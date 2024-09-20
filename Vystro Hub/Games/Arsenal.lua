a352f241152f150f655f3765533b78a59ed1133f
smhaa <166930057+Chromeyc@users.noreply.github.com>
2024-09-20 21:30:18 +0200

    Update Arsenal.lua

--- Vystro Hub/Games/Arsenal.lua
+++ Vystro Hub/Games/Arsenal.lua
@@ -1,2 +1,23 @@
 -- Not Added Yet
-print("Nothing")
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+    Title = 'Vystro | Arsenal 1.0',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['Main'] = Window:AddTab('Main'),
+}
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Note:')
+
+LeftGroupBox:AddLabel('This Script is not added Yet!')
